676b593e018202415d2c1b2d31e2b3d7f089fa58
LocalHostBit <growlyseo.com@gmail.com>
2020-10-07 16:53:50 +0200

    Updates

---
+++ precache-manifest.d4743671890a330166b387405328fb89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7516cec999b10f38111a7db6627d6c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "726c611a6cff3747dcb5",
+    "url": "/static/css/main.9f5f06de.chunk.css"
+  },
+  {
+    "revision": "b31f7acab27206a05f11",
+    "url": "/static/js/2.4cc363b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cc363b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726c611a6cff3747dcb5",
+    "url": "/static/js/main.4a57fc5a.chunk.js"
+  },
+  {
+    "revision": "0e76490f2d781cf1b0d7",
+    "url": "/static/js/runtime-main.63275c44.js"
+  },
+  {
+    "revision": "7891d075d176452afb4047a086ea5561",
+    "url": "/static/media/breakingbad.7891d075.png"
+  }
+]);
